88983207bfeb3a9c57769fc21a025c944d61544e
Martin Bradbury <martinbradbury@btinternet.com>
2024-04-18 13:03:03 +0000

    added initial state update using onClick method, eventhandler and setState

--- my-app/src/components/StatefulTest.js
+++ my-app/src/components/StatefulTest.js
@@ -8,11 +8,20 @@ class StatefulTest extends React.Component {
             end: "End Test",
         };
     }
+
+    handleClick() {
+        this.setState ({
+            end: "yo!",
+        });
+        console.log(this.state.end);
+    }
+
+
     render() {
         return (
             <div>
                 <h1>{this.state.init}This is my stateful test {this.props.pet} has an iq of {this.props.iq} {this.props.sex} </h1>
-                <button>{this.state.end}</button>
+                <button onClick={() => this.handleClick()}>{this.state.end}</button>
             </div>
     )
     }
