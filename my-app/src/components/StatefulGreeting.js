ea971ba10ad3386767d750dbac397227333d715b
Martin Bradbury <martinbradbury@btinternet.com>
2024-04-18 18:11:21 +0000

    updated state with prevState arrow function

--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -6,18 +6,33 @@ class StatefulGreeting extends React.Component {
         this.state = {
             introduction: "hello",
             buttonText: "Exit",
+            count: 0,
         };
     }
     handleClick() {
-        this.setState({
-            introduction: "Goodbye!",
-            buttonText: "Enter",
-        }, () => {
-            console.log('after', this.state.introduction);
-            console.log('after', this.state.buttonText);
+        this.setState((prevState, prevProps) => {
+            console.log('prevState', prevState)
+            console.log('prevProps', prevProps)
+            return {
+                introduction: prevState.introduction === "Hello" ? "Goodbye!" : "Hello",
+                buttonText: prevState.buttonText === "Enter" ? "Exit" : "Enter"
+            }
+        })
+    }  
 
-        });
-        
+
+    increment() {
+        this.setState((prevState, prevProps) => {
+            console.log('prevState', prevState)
+            console.log('prevProps', prevProps)
+            return {
+                count: prevState.count + 1
+            }
+
+        }
+
+
+        )
     }
 
     render() {
@@ -25,14 +40,13 @@ class StatefulGreeting extends React.Component {
             <div>
                 <h1>{this.state.introduction}, This is a stateful Greeting {this.props.greeting} welcome back {this.props.name} </h1>
                 <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
-
-
+                <button onClick={() => this.increment()}>Increment</button>
+                <p>You've clicked {this.state.count} times</p>
             </div>
 
         ) 
     }
-
-
+    
 }
 
 export default StatefulGreeting
