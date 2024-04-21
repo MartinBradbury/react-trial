9c5fce3a7cf073bd7dc47e463eab2422b3947a74
Martin Bradbury <martinbradbury@btinternet.com>
2024-04-21 21:34:32 +0000

    added conditional rendering to Stateful.js

--- my-app/src/components/Stateful.js
+++ my-app/src/components/Stateful.js
@@ -8,6 +8,7 @@ export class Stateful extends Component {
         heading: "Test",
         buttonText: "enter",
         count: 0,
+        loggedIn: false,
 
       };
     }
@@ -22,6 +23,7 @@ export class Stateful extends Component {
     Counter = () => {
         this.setState((prevState) => ({
             count: prevState.count + 1,
+            loggedIn: prevState.loggedIn === false ? true : false,
         }), () => {
             console.log(this.state.count);
         });
@@ -37,6 +39,12 @@ export class Stateful extends Component {
         <h1>{this.state.heading}</h1>
         <button onClick={(this.theClick)}>{this.state.buttonText}</button>
         <button onClick={(this.Counter)}>{this.state.count}</button>
+        <h1>{this.state.loggedIn ? "logged In" : "Not Logged In"}</h1>
+        {this.state.loggedIn ? (
+            <p>"you are logged in"</p>
+        ) : (
+            <p>"Not logged in"</p>
+        )}
       </div>
     )
   }
